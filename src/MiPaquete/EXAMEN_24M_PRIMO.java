709b6b5ffb6edf5b54f6e8633fae952ee38c8cd1
angelmartorellbedoya <angelmartorellbedoya@10.10.11.18>
2021-03-24 11:26:49 +0100

    Modificacion

---
+++ src/MiPaquete/EXAMEN_24M_PRIMO.java
@@ -0,0 +1,12 @@
+package MiPaquete;
+
+import java.util.Scanner;
+
+public class EXAMEN_24M_PRIMO {
+	public static void main(String args[]) {
+		int num=0;
+		Scanner ent = new Scanner(System.in);
+		System.out.println("Hola, introduce un número por teclado para saber si es primo");
+	}
+
+}
